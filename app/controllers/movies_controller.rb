0dc60d17cb3d17a27168fdc52845c28fa294a781
Tom Hsu <tomhsu@berkeley.edu>
2022-10-04 00:08:39 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-
     def show
       id = params[:id] # retrieve movie ID from URI route
       @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +6,9 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      @all_ratings = Movie.all_ratings
+      @ratings_to_show = params[:ratings].keys
+      @movies = Movie.with_ratings(@ratings_to_show)
     end
   
     def new
